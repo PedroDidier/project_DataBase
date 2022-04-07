db6a0abbdf3f8ae72216867e9d9404a1daaa1817
Pedro Lemos <pedrotlemos@gmail.com>
2022-04-07 19:39:35 -0300

    Alteracoes finais

--- Entrega1/consultas_plsql.sql
+++ Entrega1/consultas_plsql.sql
@@ -1,5 +1,6 @@
--- 1.USO DE RECORD + 3.BLOCO ANÔNIMO
--- Vamos utilizar o RECORD para criar um record type_pessoa e a inserir
+/* 1.USO DE RECORD & 3.BLOCO ANÔNIMO */
+/* Criar um RECORD com estrutura igual à de Pessoa, instanciar um exemplo e inserí-lo na tabela */
+
 DECLARE TYPE type_pessoa IS RECORD(
     cpf varchar(14),
     nome varchar(20),
@@ -13,11 +14,10 @@ BEGIN
     INSERT INTO pessoa VALUES pessoa_1;
 END;
 
+/* 2.ESTRUTURA TIPO TABLE, 7.%ROWTYPE, 8.IF ELSIF & 12. FOR IN LOOP */
+/* Declararemos uma lista de linhas de funcionario e checaremos se alguém está recebendo menos do que deveria
+ Além disso checamos se a pessoa é o CEO e adicionamos à fila com um salário modificado */
 
-
--- 2.ESTRUTURA TIPO TABLE + 7.%ROWTYPE + 8.IF ELSIF + 12. FOR IN LOOP
--- Declararemos uma lista de linhas de funcionario e checaremos se alguém está recebendo menos do que deveria
--- Além disso checamos se a pessoa é o CEO e adicionamos à fila com um salário modificado
 DECLARE TYPE linha_de_funcionario IS TABLE OF funcionario%ROWTYPE;
 
 lista linha_de_funcionario := linha_de_funcionario();
@@ -30,12 +30,12 @@ BEGIN
 
 		IF empregado.renda < 1000 THEN
 
-            lista(lista.LAST).cpf_p := individuo.cpf_p;
-            lista(lista.LAST).renda := individuo.renda;
+            lista(lista.LAST).cpf_p := empregado.cpf_p;
+            lista(lista.LAST).renda := empregado.renda;
 
-		ELSIF individuo.cargo LIKE 'CEO%' THEN
+		ELSIF empregado.cargo LIKE 'CEO%' THEN
 
-			lista(lista.LAST).cpf_p := cpf_p;
+			lista(lista.LAST).cpf_p := empregado.cpf_p;
 			lista(lista.LAST).renda := 50000.50;
 
 		END IF;
@@ -44,82 +44,70 @@ BEGIN
 
 END;
 
+/* 4. CREATE PROCEDURE + 6.%TYPE + 9.CASE WHEN + 17.CREATE OR RECPLACE PACKAGE + 18.CREATE OR RECPLACE PACKAGE BODY */
+/* PACKAGE de procedimentos de aumento. Procedure para aumento de gerentes e CEOs e um para aumento de faxineiros e entregadores */
 
-
--- 4. CREATE PROCEDURE + 6.%TYPE + 9.CASE WHEN + 17.CREATE OR RECPLACE PACKAGE + 18.CREATE OR RECPLACE PACKAGE BODY
--- PACKAGE de procedimentos de aumento. Procedure para aumento de gerentes e CEOs e um para aumento de faxineiros e entregadores
 CREATE OR REPLACE PACKAGE aumentos AS
     PROCEDURE aumento_lideres(
-        gerente_renda := funcionario.renda%TYPE;
-        ceo_renda := funcionario.renda%TYPE;
+        gerente_renda funcionario.renda%TYPE,
+        ceo_renda funcionario.renda%TYPE
     );
     PROCEDURE aumento_trabalhadores(
-        entregador_renda := funcionario.renda%TYPE;
-        faxineiro_renda := funcionario.renda%TYPE;
+        entregador_renda funcionario.renda%TYPE,
+        faxineiro_renda funcionario.renda%TYPE
     );
 END aumentos;
 
 CREATE OR REPLACE PACKAGE BODY aumentos AS
-    PROCEDURE ROCEDURE aumento_lideres(
-        gerente_renda := funcionario.renda%TYPE;
-        ceo_renda := funcionario.renda%TYPE;
-    ) 
-        IS BEGIN
-            FOR empregado IN (SELECT * FROM funcionario F) LOOP
+    PROCEDURE aumento_lideres ( gerente_renda funcionario.renda%TYPE, ceo_renda funcionario.renda%TYPE) IS 
+    BEGIN
+        FOR empregado IN (SELECT * FROM funcionario F) LOOP
 
-                CASE empregado.cargo
-                    WHEN 'Gerente' THEN
-                        empregado.renda := gerente_renda;
-                    WHEN 'CEO' THEN
-                        empregado.renda := ceo_renda;
-                    ELSE
-                        empregado.renda := empregado.renda + 100;
-                END CASE;
+            CASE empregado.cargo
+                WHEN 'Gerente' THEN
+                    empregado.renda := gerente_renda;
+                WHEN 'CEO' THEN
+                    empregado.renda := ceo_renda;
+                ELSE
+                    empregado.renda := empregado.renda + 100;
+            END CASE;
 
-            END LOOP;
-
-        END;
-
+        END LOOP;
+    
     END aumento_lideres;
 
-    PROCEDURE ROCEDURE aumento_trabalhadores(
-        entregador_renda := funcionario.renda%TYPE;
-        faxineiro_renda := funcionario.renda%TYPE;
-    ) 
-        IS BEGIN
-            FOR empregado IN (SELECT * FROM funcionario F) LOOP
+    
 
-                CASE empregado.cargo
-                    WHEN 'Entregador' THEN
-                        empregado.renda := entregador_renda;
-                    WHEN 'Faxineiro' THEN
-                        empregado.renda := faxineiro_renda;
-                    ELSE
-                        empregado.renda := empregado.renda + 0;
-                END CASE;
+    PROCEDURE aumento_trabalhadores(entregador_renda funcionario.renda%TYPE, faxineiro_renda funcionario.renda%TYPE) IS 
+    BEGIN
+        FOR empregado IN (SELECT * FROM funcionario F) LOOP
 
-            END LOOP;
+            CASE empregado.cargo
+                WHEN 'Entregador' THEN
+                    empregado.renda := entregador_renda;
+                WHEN 'Faxineiro' THEN
+                    empregado.renda := faxineiro_renda;
+                ELSE
+                    empregado.renda := empregado.renda + 0;
+            END CASE;
 
-        END;
-        
+        END LOOP;
+
     END aumento_trabalhadores;
-
+    
 END aumentos;
+/* 5.CREATE FUNCTION, 10.LOOP EXIT WHEN, 14.CURSOR (OPEN,FETCH e CLOSE) + 15.EXCEPTION WHEN */
+/* Função que retorna o valor mais barato de um produto de uma categoria X (input), e imprime todos os outros */
 
-
-
--- 5.CREATE FUNCTION + 10.LOOP EXIT WHEN + 14.CURSOR (OPEN,FETCH e CLOSE) + EXCEPTION WHEN
--- Função que retorna o valor mais barato de um produto de uma categoria X (input), e imprime todos os outros
-DECLARE
-    TYPE infos_prod IS RECORD(
-        nome_item VARCHAR(30),
-        preco DECIMAL(7,2),
-        quantidade INT
-    );
-
-CREATE OR REPLACE FUNCTION consultar_produto
-    (categoria_prod produto.categoria%TYPE)
-    RETURN infos_prod AS infos_do_produto
+CREATE OR REPLACE TYPE infos_prod IS OBJECT(
+    nome_item VARCHAR(30),
+    preco DECIMAL(7,2),
+    quantidade INT
+);
+CREATE OR REPLACE FUNCTION consultar_produto (categoria_prod produto.categoria%TYPE)
+    RETURN infos_prod IS
+    
+    infos_do_produto infos_prod;
 
     CURSOR cursor_produto IS
         SELECT P.nome_do_produto, P.preco, P.quantidade
@@ -149,84 +137,38 @@ CREATE OR REPLACE FUNCTION consultar_produto
 
 END consultar_produto;
 
-
-
--- 11.While LOOP + 16.Uso de Parâmetros IN, OUT ou  IN OUT
--- Bloco de codigo que checa quantos pedidos existem com um produto de livro ou papelaria
+/* 11.While LOOP & 16.Uso de Parâmetros IN, OUT ou IN OUT */
+/* Faz um "print" de uma frase indicando os funcionários que entregaram os produtos 
+"Livro Harry potter" e "Caneta bic" com seus respectivos CPFs */
 DECLARE
     cnt INTEGER;
-    i INTEGER;
     q INTEGER;
     aux_nome pedido.nome_produto%TYPE;
-    aux_empresa pedido.cnpj_fornecedor_produto%TYPE;
-    aux_cat produto.nome%TYPE;
-    aux_q produto.quantidade%TYPE;
-
+    aux_cpf pedido.cpf_e%TYPE;
+    aux_n pessoa.nome%TYPE;
+    iter INT;    
 BEGIN
-    aux_count := 0;
-    i := 1;
-    SELECT COUNT(*) INTO q FROM produto;
-    WHILE i <= q LOOP
+    iter := 1;
+    cnt := 0;
+    SELECT COUNT(*) INTO q FROM pedido;
+    WHILE iter <= q LOOP
+        
+        SELECT p.nome_produto INTO aux_nome
+        FROM pedido p
+        WHERE p.id_ = iter;
+        
+        SELECT p.cpf_e INTO aux_cpf
+        FROM pedido p
+        WHERE p.id_ = iter;
 
-        SELECT x.nome_produto INTO aux_nome
-        FROM pedido x
-        WHERE x.id_ = i;
-
-        SELECT y.cnpj_fornecedor_produto INTO aux_empresa
-        FROM pedido y
-        WHERE y.id_ = i;
-
-        SELECT z.categoria INTO aux_cat
-        FROM produto z
-        WHERE z.cnpj_fornecedor = aux_empresa
-        AND z.nome_do_produto = aux_nome
-
-        IF aux_cat in ('Livro','Papelaria') THEN
-
-            SELECT z.categoria INTO aux_q
-            FROM produto k
-            WHERE k.cnpj_fornecedor = aux_empresa
-            AND k.nome_do_produto = aux_nome
-            AND k.categoria = aux_cat
-
-            cnt := cnt + aux_q;
+        IF aux_nome IN ('Livro Harry potter', 'Caneta bic') THEN
+            SELECT p.nome INTO aux_n
+            FROM pessoa p
+            WHERE p.cpf = aux_cpf;
+            DBMS_OUTPUT.PUT_LINE(''||aux_n||' '||aux_cpf||'');
         END IF;
 
-        i := i + 1;
+        iter := iter + 1;
 
     END LOOP;
-    DBMS_OUTPUT.PUT_LINE('A transportadora está transportando '|| cnt || ' produto(s) de livro ou papelaria');
-END aux1;
-
-
-
--- 13.Select INTO + 19.Create or replace trigger (comando)
--- Cria um trigger que alerta quantos produtos já foram registrados na transportadora
-CREATE OR REPLACE TRIGGER produto_repetido
-    BEFORE INSERT ON produto
-        DECLARE produtos INTEGER;
-    BEGIN
-        SELECT COUNT(*) INTO produtos
-        FROM produto;
-
-        produtos := produtos + 1;
-
-        DBMS_OUTPUT.PUT_LINE('A transportadora possui acesso a '|| produtos || ' produto(s)');
-    END;
-
-
-    
--- 20.Create or replace trigger (linha)
--- Cria um trigger que levanta uma exception quando se tenta deletar o CEO da empresa
-CREATE OR REPLACE TRIGGER delceo
-    BEFORE DELETE ON funcionario
-        FOR EACH ROW
-            DECLARE deletar_CEO EXCEPTION;
-            BEGIN
-                IF :OLD.cargo='CEO' THEN
-                    RAISE deletar_ceo;
-                END IF;
-            EXCEPTION
-                WHEN deletar_ceo THEN
-                    RAISE_APPLICATION_ERROR(-20011, 'Nao se pode deletar um CEO da empresa.');
-            END;
+END;
--- Entrega1/consultas_sql.sql
+++ Entrega1/consultas_sql.sql
@@ -1,106 +1,96 @@
-/* 1. ALTER TABLE
-Descrição: Atualizar o campo telefone da tabela telefone_fornecedor, para usar VARCHAR(11) ao invés de INT para facilitar filtros como buscar por DDD. */
+/* 1. ALTER TABLE */
+/* Atualizar o campo telefone da tabela telefone_fornecedor, para usar VARCHAR(11) ao invés de INT para facilitar filtros como buscar por DDD. */
 ALTER TABLE telefone_fornecedor
 MODIFY (numero_de_telefone VARCHAR(11));
 
-/* 2.CREATE INDEX
-Descrição: Criar um índice para o atributo supervisionado_por na tabela funcionario, para possibilitar consultas rapidas de subordinados. */ 
+/* 2.CREATE INDEX */
+/* Criar um índice para o atributo supervisionado_por na tabela funcionario, para possibilitar consultas rapidas de subordinados. */ 
 CREATE INDEX indice_supervisionado_por
 ON funcionario (supervisionado_por);
 
-/* 3. INSERT INTO
-Descrição: Inserção de uma nova pessoa na tabela pessoa. */
-INSERT INTO pessoa VALUES ('111.111.111-21', 'Fulano', to_date('01/01/1978', 'dd/mm/yyyy'));
-INSERT INTO funcionario VALUES ('111.111.111-21', NULL, 'Gerente', 600, to_date('01/01/2001', 'dd/mm/yyyy'));
-
-/* 4. UPDATE
-Descrição: Atualizar o campo renda de um funcionário na tabela funcionário. */
+/* 3. & 4. INSERT INTO & UPDATE */
+/* Inserção de uma nova pessoa na tabela pessoa e em seguida atualização de sua renda. */
+INSERT INTO pessoa VALUES ('111.111.111-31', 'Fulano', to_date('01/01/1978', 'dd/mm/yyyy'));
+INSERT INTO funcionario VALUES ('111.111.111-31', NULL, 'Gerente', 8000, to_date('01/01/2022', 'dd/mm/yyyy'));
 UPDATE funcionario
-SET renda=10000.00
+SET renda=8000.00
 WHERE cpf_p='111.111.111-21';
 
-/* 5. DELETE 
-Descrição: Deletar todas as ocorrências na tabela produtos, cujo cnpj seja igual a  '11.111.111/0001-11',
+/* 5. DELETE */ 
+/* Deletar todas as ocorrências na tabela produtos, cujo cnpj seja igual a  '11.111.111/0001-11',
 caso a entregadora tenha parado de usar os serviços. */
 DELETE FROM produto
 WHERE (cnpj_fornecedor = '11.111.111/0001-11');
 
-/* 6 e 7. SELECT-FROM-WHERE & BETWEEN 
-Descrição: Selecionar a data de admissão e o cpf da pessoa das ocorrências da tabela funcionario
- onde a data de admissão está entre 12:00 de 01 de janeiro de 2021 e 12:00 de 01 de janeiro de 2022. */
-SELECT cpf_p, data_de_admissao FROM funcionario
-WHERE TO_TIMESTAMP(data_de_admissao)
-BETWEEN TO_TIMESTAMP('01-JAN-2000 12.00.00.000000 PM') AND TO_TIMESTAMP('01-JAN-2002 12.00.00.000000 PM');
+/* 6. SELECT-FROM-WHERE, 7. BETWEEN & 21. ORDER BY */ 
+/* Selecionar o cpf e a renda dos funcionários que possuem renda entre 5000 e 10000 reais, mostrando a renda em ordem crescente */
+SELECT cpf_p, renda FROM funcionario
+WHERE renda BETWEEN 5000 AND 10000
+ORDER BY renda;
 
-/* 8. IN
-Descrição: Selecionar os CPFs das dos funcionarios das ocorrências da tabela funcionario,
- tais que o funcionário tenha tenha um supervisor com o CPF '111.111.111-12' ou '111.111.111-13' */
-SELECT cpf_p, supervisionado_por FROM funcionario
+/* 8. IN */ 
+/* Selecionar os CPFs dos funcionarios que tenham como supervisor alguém com o CPF '111.111.111-12' ou '111.111.111-21' */
+SELECT cpf_p FROM funcionario
 WHERE supervisionado_por IN ('111.111.111-12', '111.111.111-21');
 
-/* 9. LIKE 
-Selecionar, na tabela Pessoa, o nome e data de nascimento de todas as pessoas que tem o nome começando com ‘F’. */
-SELECT nome, data_de_nascimento FROM pessoa
-WHERE nome LIKE 'F%';
+/* 9. LIKE */
+/* Selecionar o nome, O CNPJ do fornecedor e o preço de todos os produtos que têm o nome começando com a letra L. */
+SELECT nome_do_produto, cnpj_fornecedor, preco FROM produto
+WHERE nome_do_produto LIKE 'L%';
 
-/* 10. IS NULL OU IS NOT NULL
-Descrição: Buscar o nome dos funcionários que não possuem nenhum supervisor. */
-SELECT P.nome from funcionario F, pessoa P
-WHERE F.cpf_p = P.cpf AND F.supervisionado_por IS NULL;
+/* 10. IS NULL ou IS NOT NULL */ 
+/* Selecionar o nome e o CPF do supervisor correspondentes aos funcionários com um supervisor ativo. */
+SELECT P.nome, F.supervisionado_por 
+FROM funcionario F, pessoa P
+WHERE F.cpf_p = P.cpf AND F.supervisionado_por IS NOT NULL;
 
-/* 11. INNER JOIN
-Descrição: Buscar o nome na tabela pessoa e CPF
-dos funcionários que supervionam na tabela funcionario, sem admitir valores duplicados. */
-SELECT DISTINCT P.nome FROM pessoa P
+/* 11. INNER JOIN */ 
+/* Selecionar o nome dos funcionários que também são supervisores */
+SELECT DISTINCT P.nome 
+FROM pessoa P
 INNER JOIN funcionario F
 ON P.cpf = F.supervisionado_por;
 
-/* 12 & 13 & 14 . MAX, MIN, AVG
-Descrição: Encontrar a maior renda, a menor renda,
-e a renda média entre todos os funcionários da tabela funcionario. */
+/* 12. MAX, 13. MIN & 14. AVG */
+/* Calcular a maior, a menor e a renda média dos funcionários. */
 SELECT MAX(renda), MIN(renda), AVG(renda)
 FROM funcionario;
 
-/* 15. COUNT
-Descrição: Encontrar, na tabela funcionario, o número de funcionários que possuem uma renda acima da media. */
-SELECT COUNT(*)
-FROM funcionario
-WHERE (renda > (SELECT AVG(renda) from funcionario));
-
-/* 16. LEFT OU RIGHT OU FULL OUTER JOIN
-Descrição: Consultar todos os funcionários, mostrando seu CPF,
-e mostrar o CEP de entrega daqueles que também são destinatarios. */
-SELECT F.cpf_p, D.cep
+/* 15. COUNT & 16. LEFT ou RIGHT ou FULL OUTER JOIN */ 
+/* Encontrar o número de funcionários que foram contratados em 2022 e também são destinatários(clientes). */
+SELECT Count(*) 
 FROM funcionario F LEFT OUTER JOIN destinatario D
-ON F.cpf_p = D.cpf_p;
+ON F.cpf_p = D.cpf_p
+WHERE (to_date(data_de_admissao) >= to_date('01/01/2022', 'dd/mm/yyyy'));
 
-/* 17 & 18. SUBCONSULTA COM OPERADOR RELACIONAL & SUBCONSULTA COM IN
-Descrição: Buscar o CPF de todos os funcionários que nasceram em 01/01/1978 ou
-em 23/04/1979. */
-SELECT F.cpf_p FROM funcionario F
-WHERE F.cpf_p IN (SELECT cpf
-FROM pessoa WHERE to_date(data_de_nascimento) = to_date('01/01/1978', 'dd/mm/yyyy') OR
-to_date(data_de_nascimento) = to_date('23/04/1979', 'dd/mm/yyyy'));
+/* 17 & 18. SUBCONSULTA COM OPERADOR RELACIONAL & SUBCONSULTA COM IN */
+/* Selecionar o nome dos fornecedores que possuem o produto 'Livro Harry Potter' numa quantidade maior que 10 unidades. */
+SELECT C.nome_da_empresa 
+FROM cnpj_nome_empresa C
+WHERE C.cnpj IN (
+    SELECT cnpj_fornecedor
+    FROM produto 
+    WHERE (nome_do_produto = 'Livro Harry potter') AND (quantidade > 10)
+);
 
-/* 19. SUBCONSULTA COM ANY
-Descrição: Listar o cpf dos funcionários que possuem salário maior
-ou igual do que o salário de pelo menos um funcionário que é supervisor. */
-SELECT cpf_p FROM funcionario WHERE
-renda >= ANY
-(SELECT renda FROM funcionario WHERE cpf_p IN 
-(SELECT supervisionado_por FROM funcionario));
+/* 19. SUBCONSULTA COM ANY */ 
+/* Selecionar os CPFs e nomes dos funcionários que estão há mais tempo na empresa que algum funcionário supervisor */
+SELECT F.cpf_p, P.nome FROM funcionario F, pessoa P
+WHERE F.cpf_p = P.cpf AND to_date(data_de_admissao) < ANY (
+    SELECT DISTINCT to_date(F.data_de_admissao) 
+    FROM funcionario F
+    INNER JOIN pessoa P
+    ON F.supervisionado_por = P.cpf
+);
 
-/* 20. SUBCONSULTA COM ALL
-Descrição: Listar o cpf dos funcionários supervisores que possuem um salário
- maior ou igual do que o salário de todos os outros funcionários. */
-SELECT cpf_p FROM funcionario WHERE
-cpf_p IN (SELECT supervisionado_por FROM funcionario) AND
-renda >= ALL (SELECT renda FROM funcionario);
-
-/* 21. ORDER BY
-Descrição: Listar o nome e preco de todos os produtos em ordem crescente. */
-SELECT nome_do_produto, preco FROM produto
-ORDER BY preco;
+/* 20. SUBCONSULTA COM ALL */
+/* Selecionar os CPFs e nomes dos funcionários que recebem mais que todos com o cargo de 'gerente' */
+SELECT F.cpf_p, P.nome FROM funcionario F, pessoa P
+WHERE F.cpf_p = P.cpf AND F.renda > ALL (
+    SELECT renda
+    FROM funcionario
+    WHERE (cargo = 'gerente')
+);
 
 /* 22. GROUP BY
 Descrição: Mostra a média de preço dos produtos de um determinado fornecedor */
@@ -117,17 +107,21 @@ GROUP BY cnpj_fornecedor
 HAVING AVG(preco) > 10;
 
 /* 24. UNION ou INTERSECT ou MINUS
-Encontrar o nome de todos os funcionários que já utilizaram o serviço. */
-SELECT nome FROM pessoa
-WHERE cpf IN (SELECT cpf_p FROM funcionario INTERSECT SELECT cpf_p FROM destinatario);
+Encontrar o nome de todos os funcionários que nunca foram destinatários(clientes). */
+SELECT nome, cpf FROM pessoa
+WHERE cpf IN (
+    SELECT cpf_p FROM funcionario 
+    MINUS
+    SELECT cpf_p FROM destinatario
+);
 
-/* 25. CREATE VIEW
-Criar uma visão para funcionarios sem supervisor */
+/* 25. CREATE VIEW */
+/* Criar uma visão para funcionarios sem supervisor */
 CREATE VIEW funcionarios_supervisores AS
 SELECT * FROM funcionario
 WHERE supervisionado_por = NULL;
 
--- 26. GRANT & REVOKE 
+/* 26. GRANT & REVOKE */ 
 
 -- Cria usuário Didier para alterar permissões
 CREATE USER Didier IDENTIFIED BY Didier;
