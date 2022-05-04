c008f92428e5745e0809977a0fdb9be75e38aaab
Nilo Drumond <nilobdrumond@gmail.com>
2022-05-04 19:37:14 -0300

    consultas a VARRAY e a NESTED TABLE

--- Entrega2/consultas_OR.sql
+++ Entrega2/consultas_OR.sql
@@ -3,7 +3,15 @@ SELECT F.nome, F.endereco.CEP
 FROM tab_fornecedor F;
 
 -- (2. SELECT DREF)
--- Seleciona as informacoes prinicipais dos pedidos em que se enviou um produto para Arthur Cassimiro, sendo entregue por Pedro Didier
+-- Seleciona as informacoes principais dos pedidos em que se enviou um produto para Arthur Cassimiro, sendo entregue por Pedro Didier
 SELECT PED.id, PED.data_pedido, PED.data_entrega, PED.frete, PED.forma_de_pagamento
 FROM tab_pedido PED
 WHERE DEREF(PED.entregador_ped).nome = 'Pedro Didier' AND DEREF(PED.destinatario_ped).nome = 'Arthur Cassimiro';
+
+-- (3. CONSULTA À NESTED VARRAY)
+-- Seleciona o nome, cnpj e os telefones de um fornecedor
+SELECT F.nome, F.CNPJ, T.*
+FROM tab_fornecedor F, TABLE(F.telefones) T;
+
+SELECT F.nome, P.quantidade
+FROM tab_fornecedor F OUTER APPLY (SELECT COUNT(*) AS quantidade from TABLE(produtos_possuidos)) P
